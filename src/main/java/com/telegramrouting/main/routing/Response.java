aa3f06fc9e5241b430bedfe8ceb74658be07b583
Evemose <4upa4undra@gmail.com>
2023-12-01 18:15:59 +0200

    Init

--- src/main/java/com/telegramrouting/main/routing/Response.java
+++
@@ -1,8 +0,0 @@
-package com.telegramrouting.main.routing;
-
-import lombok.Builder;
-import org.telegram.telegrambots.meta.api.methods.botapimethods.BotApiMethodMessage;
-
-@Builder
-public record Response(BotApiMethodMessage... messages) {
-}
