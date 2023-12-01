ec646d6117b0faae9c96f3ce2cc9505199f37587
Evemose <4upa4undra@gmail.com>
2023-12-01 18:15:20 +0200

    Init

--- src/main/java/com/telegramrouting/main/routing/routers/DynamicRouter.java
+++
@@ -1,6 +0,0 @@
-package com.telegramrouting.main.routing.routers;
-
-public interface DynamicRouter extends Router {
-    void register(String command, HandlerMethodInfo handlerMethodInfo);
-    void deregister(String command);
-}
